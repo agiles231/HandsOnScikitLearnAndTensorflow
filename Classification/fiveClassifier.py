17b87108da2be980ec913fa1b344f9a5cab7c100
Andrew Giles <agiles231@gmail.com>
2019-12-05 08:40:04 -0500

    Exploring model metrics

--- Classification/fiveClassifier.py
+++ Classification/fiveClassifier.py
@@ -1,10 +1,17 @@
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 import pullMnist as pm
 import fold
 import split_train_test as stt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
 from sklearn.base import BaseEstimator
 import numpy as np
 
@@ -33,3 +40,52 @@ cvs = cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 print(cvs)
 y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 print(confusion_matrix(y_train_5, y_train_pred))
+print(precision_score(y_train_5, y_train_pred))
+print(recall_score(y_train_5, y_train_pred))
+print(f1_score(y_train_5, y_train_pred))
+
+y_scores = sgd_clf.decision_function([some_digit])
+threshold = 0
+y_some_digit_pred = (y_scores > threshold)
+print(y_some_digit_pred)
+threshold = 200000
+y_some_digit_pred = (y_scores > threshold)
+print(y_some_digit_pred)
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
+
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="center left")
+    plt.ylim([0, 1])
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.show()
+y_train_pred_90 = (y_scores > 70000)
+print(precision_score(y_train_5, y_train_pred_90))
+print(recall_score(y_train_5, y_train_pred_90))
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0,1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel("True positive Rate")
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+print(roc_auc_score(y_train_5, y_scores))
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+plt.plot(fpr, tpr, "b:", label="SGD")
+plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+plt.legend(loc="lower right")
+plt.show()
+print(roc_aus_score(y_train_5, y_scores_forest))
+sgd_clf_fit(X_train, y_train)
+sgd_clf.predict([some_digit])
